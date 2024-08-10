d22ffcbbea9dec20b5e34bc1fff230b3ebbffdcf
joegenequ <joegenequesada@gmail.com>
2024-08-10 14:34:47 +0000

    Toggle isFavorite

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -1,17 +1,56 @@
 <template>
   <div>
-    <h2>{{ foodName }}</h2>
+    <h2>
+      {{ foodName }}
+      <img src="/img_quality.svg" v-show="foodIsFavorite">
+    </h2>
     <p>{{ foodDesc }}</p>
+    <button v-on:click="toggleFavorite">Favorite</button>
   </div>
 </template>
 
 <script>
   export default {
-    props: [
-      'foodName',
-      'foodDesc'
-    ]
-  }
+        props: {
+           foodName: {
+                type: String,
+                required: true
+            },
+           foodDesc: {
+             type: String,
+             required: false,
+             default: 'This is the default description.',
+             validator: function(value) {
+               if( 20<value.length && value.length<50 ) {
+                 return true;
+               }
+               else {
+                 return false;
+               }
+             }
+           },
+           isFavorite: {
+             type: Boolean,
+             required: false,
+             default: false
+             }
+        },
+        data() {
+          return {
+            foodIsFavorite: this.isFavorite
+          }
+        },
+        methods: {
+          toggleFavorite() {
+            this.foodIsFavorite = !this.foodIsFavorite;
+          }
+        }           
+    }
 </script>
 
-<style></style>
+<style>
+  img {
+    height: 1.5em;
+    float: right;
+  }
+</style>
