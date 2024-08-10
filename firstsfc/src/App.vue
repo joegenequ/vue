706d2af9bac4213b709a8f6af2a413ba5d7c2be4
joegenequ <joegenequesada@gmail.com>
2024-08-10 13:33:19 +0000

    Receive data inside a component

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <h1>Food</h1>
-  <food-item/>
-  <food-item/>
-  <food-item/>
+  <div id="wrapper">
+    <food-item
+      food-name="Apples"
+      food-desc="Apples are a type of fruit that grow on trees."/>
+    <food-item
+      food-name="Pizza"
+      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."/>
+    <food-item
+      food-name="Rice"
+      food-desc="Rice is a type of grain that people like to eat."/>
+  </div>
 </template>
 
 <script></script>
 
 <style>
-#app > div:hover {
-  cursor: pointer;
-}
-  #app > div {
+  #wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #wrapper > div {
     border: dashed black 1px;
-    display: inline-block;
     margin: 10px;
     padding: 10px;
     background-color: lightgreen;
